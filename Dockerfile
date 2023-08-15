f1c89c60b9d526af7ac0a7e6409fcb0f56cd58c5
Jakub Augustyniak <kubaugust@gmail.com>
2023-08-15 10:47:35 +0200

    Node env fixes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:alpine AS builder
-ENV NODE_ENV prod
+ENV NODE_ENV production
 WORKDIR /app
 COPY ./package.json ./
 RUN npm install
--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 const config = {
-  serverBaseUrl: process.env.REACT_APP_BACKEND_URL,
-  googleApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  serverBaseUrl: process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : process.env.REACT_APP_BACKEND_URL,
+  googleApiKey: process.env.REACT_APP_GOOGLE_API_KEY
 };
 
 export default config;
